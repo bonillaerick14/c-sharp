853ee70b79614c8cb30ea1cf3bea4493edaeed10
Erick Garcia <garciaerickbonilla@gmail.com>
2022-06-29 21:33:11 -0600

    inventario

---
+++ Ejercicios/inventario/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace holamundo
+{
+  class Program
+  {
+
+   static void Main(string[] args)
+
+   {
+    int opcion = 0;
+    while (true)
+    {
+      Console.Clear();
+      Console.WriteLine("Sistema de Inventario");
+      Console.WriteLine("*********************");
+      Console.WriteLine("");
+      Console.WriteLine("1 - productos");
+      Console.WriteLine("2 - Ingreso de Inventario");
+      Console.WriteLine("3 - Salida de Inventario");
+      Console.WriteLine("0 - Salir");
+      opcion = Console.read();
+
+      switch (opcion)
+      {
+        case 1:
+        Console.WriteLine("Productos!");
+        Console.ReadLine();
+        break;
+        default:
+        break;
+      }
+
+      if (opcion == 0) {
+        break;
+      }
+    }
+    
+   }
+
+
+  }
+
+
+}
