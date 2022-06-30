e9d47de94f16854342849500bf431946ae2a4718
Erick Garcia <garciaerickbonilla@gmail.com>
2022-06-29 22:41:40 -0600

    cambios

--- Ejercicios/inventario/Program.cs
+++ Ejercicios/inventario/Program.cs
@@ -1,45 +1,158 @@
 ﻿using System;
 
-namespace holamundo
+namespace inventario
 {
-  class Program
-  {
-
-   static void Main(string[] args)
-
-   {
-    int opcion = 0;
-    while (true)
+    class Program
     {
-      Console.Clear();
-      Console.WriteLine("Sistema de Inventario");
-      Console.WriteLine("*********************");
-      Console.WriteLine("");
-      Console.WriteLine("1 - productos");
-      Console.WriteLine("2 - Ingreso de Inventario");
-      Console.WriteLine("3 - Salida de Inventario");
-      Console.WriteLine("0 - Salir");
-      opcion = Console.read();
+        static string[,] productos = new string[5,3]
+        {
+            { "001", "iPhoneX", "0" },
+            { "002", "Laptop Dell", "5" },
+            { "003", "Monitor Samsung", "2" },
+            { "004", "Mouse", "100" },
+            { "005", "Headset", "25" },
+        };
 
-      switch (opcion)
-      {
-        case 1:
-        Console.WriteLine("Productos!");
-        Console.ReadLine();
-        break;
-        default:
-        break;
-      }
+        static void listarProductos() {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Listado de Productos");
+            Console.WriteLine("********************");
+            Console.WriteLine("Codigo, Descripcion y Existencia");
 
-      if (opcion == 0) {
-        break;
-      }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(productos[i, 0] + " | " + productos[i, 1] + " | " + productos[i, 2]);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void movimientoInventario(string codigo, int cantidad, string tipoMovimiento) {
+            for (int i = 0; i < 5; i++)
+            {
+                if (productos[i, 0] == codigo) {
+                    if (tipoMovimiento == "+") {
+                        productos[i, 2] = (Int32.Parse(productos[i, 2]) + cantidad).ToString();
+                    } else {
+                        productos[i, 2] = (Int32.Parse(productos[i, 2]) - cantidad).ToString();
+                    }
+                }
+            }
+        }
+
+        static void ingresoDeInventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("Ingreso de Productos al Inventario");
+            Console.WriteLine("**********************************");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "+");
+        }
+        
+        static void salidadeinventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("Ingreso de Productos al Inventario");
+            Console.WriteLine("**********************************");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "-");
+        }
+
+         static void ajustespositivos() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("Ingreso de Productos al Inventario");
+            Console.WriteLine("**********************************");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "-");
+        }
+
+         static void ajustesnegativos() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("Ingreso de Productos al Inventario");
+            Console.WriteLine("**********************************");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+
+            movimientoInventario(codigo, Int32.Parse(cantidad), "-");
+        }
+
+        
+        static void Main(string[] args)
+        {
+            string opcion = "";
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de Inventario");
+                Console.WriteLine("*********************");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Productos");
+                Console.WriteLine("2 - Ingreso de Inventario");
+                Console.WriteLine("3 - Salida de Inventario");
+                Console.WriteLine("4 - Ajustes Positivos");
+                Console.WriteLine("5 - Ajustes Negativos");
+                Console.WriteLine("0 - Salir");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1": 
+                        listarProductos();
+                        break;
+                    case "2":
+                        ingresoDeInventario();
+                        break;
+                    case "3":
+                        salidadeinventario();
+                    break;
+                    case "4":
+                        ajustespositivos();
+                        break;
+                    case "5":
+                        ajustesnegativos();
+                        break;
+                    default:
+                    break;
+                }
+
+                if (opcion == "0") {
+                    break;
+                }
+            }            
+        }
     }
-    
-   }
-
-
-  }
-
-
 }
